54f3788a11f9db72c705b7e77b8cf44ac52dd1c0
Daniel <daniel.arisa@yahoo.com>
2023-01-23 21:17:31 -0600

    first commit

---
+++ javascript/find_shortest_string.js
@@ -0,0 +1,35 @@
+function findShortestString(arr) {
+  arr.sort((a, b) => {
+    if (a.length < b.length) {
+      return -1;
+    } else if (a.length > b.length) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+  return arr[0];
+}
+
+if (require.main === module) {
+  // add your own tests in here
+  console.log("Expecting: 'a'");
+  console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
+
+  console.log("");
+
+  console.log("Expecting: 'hi'");
+  console.log("=>", findShortestString(['cat', 'hi', 'dog', 'an']));
+
+  console.log("");
+
+  console.log("Expecting: 'lily'");
+  console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
+
+  // BENCHMARK HERE
+}
+
+module.exports = findShortestString;
+
+// Please add your pseudocode to this file
+// And a written explanation of your solution
